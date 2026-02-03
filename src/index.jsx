3321abe0f955fea02482a2575c89928352d09a29
mayroga <132472344+mayroga@users.noreply.github.com>
2026-02-03 12:54:41 -0800

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// --- Root de la app ---
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
