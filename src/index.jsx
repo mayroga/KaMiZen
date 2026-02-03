7c1d6eed966629789843d51e0dffc0c3a3e8a6bb
mayroga <132472344+mayroga@users.noreply.github.com>
2026-02-03 15:57:46 -0800

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import LoadingScreen from "./LoadingScreen";
+
+function Root() {
+  const [ready, setReady] = useState(false);
+
+  return ready ? <App /> : <LoadingScreen onReady={() => setReady(true)} />;
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Root />);
