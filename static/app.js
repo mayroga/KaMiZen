c1b3083ed8146e1dd18f747b078a1b5db8132a2c
mayroga <132472344+mayroga@users.noreply.github.com>
2026-02-03 19:07:43 -0800

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,11 +1,8 @@
 const texts = {
- es:{},
- en:{},
- fr:{},
- de:{},
- zh:{}
+ es:{}, en:{}, fr:{}, de:{}, zh:{}
 };
 
 function setLang(l){
  document.documentElement.lang = l;
+ // Aquí podrías reemplazar todos los textos del DOM según idioma
 }
