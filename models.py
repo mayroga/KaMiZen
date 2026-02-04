9206bbe95c44da28df25a9f5acf29f0445a441a4
mayroga <132472344+mayroga@users.noreply.github.com>
2026-02-03 16:08:11 -0800

    Update models.py

--- models.py
+++ models.py
@@ -1,18 +1,11 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean
-from .database import Base
+from sqlalchemy import Column, Integer, String, DateTime, JSON
+from database import Base
 from datetime import datetime
 
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
-    created_at = Column(DateTime, default=datetime.utcnow)
+class VitalCycle(Base):
+    __tablename__ = "vital_cycles"
 
-class MicroAction(Base):
-    __tablename__ = "microactions"
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer)
-    action = Column(String)
-    done = Column(Boolean, default=False)
-    scheduled_at = Column(DateTime)
+    estado = Column(String)
+    metricas = Column(JSON)
+    created_at = Column(DateTime, default=datetime.utcnow)
