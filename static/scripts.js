057018f6ab782e11218a3da00111cb7c18b0b2f2
mayroga <132472344+mayroga@users.noreply.github.com>
2026-03-09 14:13:49 -0700

    Update scripts.js

--- static/scripts.js
+++ static/scripts.js
@@ -4,6 +4,19 @@ const block = document.getElementById("block");
 let sessionBlocks = [];
 let current = 0;
 
+let timer = null;
+let timeLeft = 120;
+
+// crear botón siguiente dinámicamente
+const nextBtn = document.createElement("button");
+nextBtn.innerText = "Siguiente";
+nextBtn.style.marginTop = "20px";
+nextBtn.style.display = "none";
+
+document.getElementById("content-box").appendChild(nextBtn);
+
+
+// función voz
 function speak(text){
     const utterance = new SpeechSynthesisUtterance(text);
     utterance.lang = "es-ES";
@@ -11,7 +24,34 @@ function speak(text){
     speechSynthesis.speak(utterance);
 }
 
+
+// temporizador
+function startTimer(){
+
+    clearInterval(timer);
+    timeLeft = 120;
+
+    timer = setInterval(()=>{
+
+        block.innerText = sessionBlocks[current-1] + "\n\nTiempo restante: " + timeLeft + "s";
+
+        timeLeft--;
+
+        if(timeLeft < 0){
+
+            clearInterval(timer);
+
+        }
+
+    },1000)
+
+}
+
+
+// mostrar bloque
 function nextBlock(){
+
+    clearInterval(timer);
 
     if(current < sessionBlocks.length){
 
@@ -23,22 +63,44 @@ function nextBlock(){
 
         current++;
 
+        startTimer();
+
         if(current < sessionBlocks.length){
-            setTimeout(nextBlock,120000); 
+
+            setTimeout(nextBlock,120000);
+
         }else{
+
             setTimeout(()=>{
+
                 block.innerText = "Sesión finalizada.";
-                speak("Sesión finalizada.");
+                speak("Sesión finalizada");
+
+                nextBtn.style.display = "none";
+
             },3000)
+
         }
 
     }
 
 }
 
+
+// botón siguiente
+nextBtn.addEventListener("click", ()=>{
+
+    nextBlock();
+
+});
+
+
+// iniciar sesión
 startBtn.addEventListener("click", async ()=>{
 
-    startBtn.style.display="none";
+    startBtn.style.display = "none";
+
+    nextBtn.style.display = "inline-block";
 
     const response = await fetch("/session_content");
 
